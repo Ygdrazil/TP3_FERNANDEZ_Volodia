50d85a1de6a3542aac80dd7322ef6f54477ce23d
Ygdrazil <vfz1204@gmail.com>
2023-02-23 02:55:38 +0100

    Added main menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,9 +19,39 @@
 			goto('/dingus');
 		}
 	);
-
-	$: sequence = konamiCode.current_sequence;
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main id="container">
+	<h1>INFO804 TP3 FERNANDEZ Volodia</h1>
+	<div id="button-list">
+		<a href="/q1"><button>Q1</button></a>
+		<a href="/q2"><button>Q2</button></a>
+	</div>
+</main>
+
+<style>
+	#container {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		align-items: center;
+		justify-content: center;
+	}
+
+	#button-list {
+		background-color: white;
+		padding: 20px;
+		border-radius: 5px;
+		display: flex;
+		flex-direction: column;
+		align-items: space-between;
+	}
+
+	@media(prefers-color-scheme: light) {
+		#button-list {
+			height: 100px;
+			background-color: #213547;
+			justify-content: space-between;
+		}
+	}
+</style>
